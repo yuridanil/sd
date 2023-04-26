74bfe1e0a0bd558f9db5b34d5262b9056165f7b2
yuridanil <33320745+yuridanil@users.noreply.github.com>
2023-04-26 14:09:15 +0300

    Add files via upload

--- worker2.py
+++ worker2.py
@@ -13,6 +13,35 @@ next_url = server_url + "/get-next"
 res_url = server_url + "/upload-result"
 sd_url = "http://127.0.0.1:7861"
 
+def makePrompt(initPrompt, params):
+    prompt = initPrompt
+
+    if "set1" in params:
+        set1 = 1
+    else:
+        set1 = 0
+    set2 = int(params["set2"])
+    set3 = int(params["set3"])
+    set4 = int(params["set4"])
+    set5 = int(params["set5"])
+
+    if set1 == 1:
+        prompt += ", a black and white portrait"
+    if set2 == 2:
+        prompt += ", bald"
+    if set2 == 3:
+        prompt += ", curly hair"
+    if set3 == 2:
+        prompt += ", in glasses"
+    if set4 == 2:
+        prompt += ", black hair"
+    if set5 == 2:
+        prompt += ", mustache"
+    if set5 == 3:
+        prompt += ", beard"
+
+    return prompt
+
 processed = 0
 id = 0
 n = 0
@@ -28,45 +57,20 @@ while n < 1000:
     if r['result'] == 'ok':
         id = r['id']
         filename = r['filename']
-        image = Image.open(BytesIO(response.content))
-        image.save('local_uploads/' + r['filename'])
-
-        parameters = json.loads(r['parameters'])
 
         print("Processing file " + filename)
-        if "set1" in parameters:
-            set1 = 1
-        else:
-            set1 = 0
-        set2 = int(parameters["set2"])
-        set3 = int(parameters["set3"])
-        set4 = int(parameters["set4"])
-        set5 = int(parameters["set5"])
         print("File id:", id)
-        print("Filename:", filename)
-        print("Params:", set1, set2, set3, set4, set5)
-        print("Starting Stable diffusion process...")
 
-        prompt = "photorealistic, masterpiece"
+        image = Image.open(BytesIO(response.content))
+        image.save('worker_input/' + r['filename'])
 
-        if set1 == 1:
-            prompt += ", A black and white portrait"
-        print(set2)
-        if set2 == 2:
-            prompt += ", bald"
-        if set2 == 3:
-            prompt += ", curly hair"
-        if set3 == 2:
-            prompt += ", in glasses"
-        if set4 == 2:
-            prompt += ", black hair"
-        if set5 == 2:
-            prompt += ", mustache"
-        if set5 == 3:
-            prompt += ", beard"
+        parameters = json.loads(r['parameters'])
+        prompt = makePrompt("photorealistic, masterpiece", parameters)
         print("Prompt:", prompt)
 
-        with open("local_uploads/" + filename, "rb") as image_file:
+        print("Starting Stable diffusion process...")
+
+        with open("worker_input/" + filename, "rb") as image_file:
             data = base64.b64encode(image_file.read())
 
         payload = {
@@ -91,12 +95,12 @@ while n < 1000:
 
             pnginfo = PngImagePlugin.PngInfo()
             pnginfo.add_text("parameters", response2.json().get("info"))
-            image.save('local_results/' + filename, pnginfo=pnginfo)
+            image.save('worker_output/' + filename, pnginfo=pnginfo)
 
         print("Processing finished")
         print("Uploadnig file...")
 
-        test_file = open("local_results/" + filename, "rb")
+        test_file = open("worker_output/" + filename, "rb")
         headers = {
             "id": id,
             "filename": filename,
